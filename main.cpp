8545c5df543466b50bcb4057cb3a07258cda2143
Eimantas Katinas <katinaseimantas@gmail.com>
2022-02-19 15:54:20 +0200

    select auto or custom width

--- main.cpp
+++ main.cpp
@@ -11,29 +11,40 @@ int main() {
     string framed_greeting;
     string greeting;
     int width;
+    char custom_width;
 
     cout << "Iveskite varda: ";
     cin >> name;
-    cout << "Iveskite remelio ploti: ";
-    cin >> width;
-    
+
     if(name.back() == 'e' || name.back() == 'a') {
         greeting = ("Sveika, " + name + "!");
     } else {
         greeting = "Sveikas, " + name + "!";
     }
 
-    try {
-        string greeting_line = "*" + string((width - 2 - greeting.length()) / 2, ' ') + greeting;
-        greeting_line += string(width - greeting_line.length() - 1, ' ') + "*\n";
+    cout << "Ar norite pasirinkti remelio ploti? (t/n): ";
+    cin >> custom_width;
+    if(custom_width == 't') {
+        cout << "Iveskite remelio ploti: ";
+        cin >> width;
+        try {
+            string greeting_line = "*" + string((width - 2 - greeting.length()) / 2, ' ') + greeting;
+            greeting_line += string(width - greeting_line.length() - 1, ' ') + "*\n";
 
-        cout << string(greeting_line.length() - 1, '*') + '\n'
-        << "*" + string(greeting_line.length() - 3, ' ') + "*\n"
-        << greeting_line
-        << "*" + string(greeting_line.length() - 3, ' ') + "*\n"
-        << string(greeting_line.length() - 1, '*') + '\n';
-    } catch (std::bad_alloc) {
-        cout << "Pasisveikinimas netelpa i remeli";
-        return 1;
+            cout << string(greeting_line.length() - 1, '*') + '\n'
+            << "*" + string(greeting_line.length() - 3, ' ') + "*\n"
+            << greeting_line
+            << "*" + string(greeting_line.length() - 3, ' ') + "*\n"
+            << string(greeting_line.length() - 1, '*') + '\n';
+        } catch (std::bad_alloc) {
+            cout << "Pasisveikinimas netelpa i remeli";
+            return 1;
+        }
+    } else {
+        cout << string(greeting.length() + 4, '*') + '\n'
+            << "*" + string(greeting.length() + 2, ' ') + "*\n"
+            << "* " + greeting + " *\n"
+            << "*" + string(greeting.length() + 2, ' ') + "*\n"
+            << string(greeting.length() +  4, '*') + '\n';
     }
 }
