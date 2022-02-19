868cb5e013449f16819a6422871c5e8c25e22689
Eimantas Katinas <katinaseimantas@gmail.com>
2022-02-19 12:50:09 +0200

    different greeting for woman

--- main.cpp
+++ main.cpp
@@ -8,12 +8,19 @@ using std::string;
 
 int main() {
     string name;
+    string greeting_line;
+
     cout << "Iveskite varda: ";
     cin >> name;
+    
+    if(name.back() == 'e' || name.back() == 'a') {
+        greeting_line = "* Sveika, " + name + "! *\n";
+    } else {
+        greeting_line = "* Sveikas, " + name + "! *\n";
+    }
 
-    string horizontal_line = string(14 + name.length(), '*') + '\n';
-    string vertical_line = "*" + string(12 + name.length(), ' ') + "*\n";
-    string greeting_line = "* Sveikas, " + name + "! *\n";
+    string horizontal_line = string(greeting_line.length() - 1, '*') + '\n';
+    string vertical_line = "*" + string(greeting_line.length() - 3, ' ') + "*\n";
     
     cout << horizontal_line << vertical_line << greeting_line << vertical_line << horizontal_line;
 }
